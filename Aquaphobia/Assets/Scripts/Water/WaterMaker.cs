8a09fc38cdc5487b722a1a0b747467bbc58d71f4
c6tonsun <toni.sundell@cs.tamk.fi>
2018-02-07 15:22:42 +0200

    Added mountain.

---
+++ Aquaphobia/Assets/Scripts/Water/WaterMaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMaker : MonoBehaviour {
+
+    public GameObject waterPrefab;
+    public int gridSize;
+
+    private void Awake()
+    {
+        for (int x = -gridSize; x < gridSize; x++)
+        {
+            for (int z = -gridSize; z < gridSize; z++)
+            {
+                GameObject waterTile = Instantiate(waterPrefab, transform);
+                waterTile.transform.position = new Vector3(x * 10, 0, z * 10);
+            }
+        }
+    }
+}
