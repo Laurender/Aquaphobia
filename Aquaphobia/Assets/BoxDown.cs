3aebf042ef342297cae4a57b8342e7e6063a7fc9
c6tonsun <toni.sundell@cs.tamk.fi>
2018-02-15 09:13:25 +0200

    Test scene update.

---
+++ Aquaphobia/Assets/BoxDown.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDown : MonoBehaviour {
+
+    public Transform end;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            transform.position = end.position;
+    }
+}
