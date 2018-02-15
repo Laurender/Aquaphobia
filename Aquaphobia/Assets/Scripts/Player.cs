075cae3b0e765f2ea21a6da6a40ab7f73ed9c40c
DESKTOP-PCRDV4T\Rottalouhio <mlouhio@hotmail.com>
2018-02-15 07:33:54 +0200

    Started doing raycast shit, added trail to player for no reason.

--- Aquaphobia/Assets/Scripts/Player.cs
+++ Aquaphobia/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Player : MonoBehaviour {
+public class Player : RaycastController {
 
     //MovementSpeed
     public float moveSpeed = 5;
@@ -19,23 +19,22 @@ public class Player : MonoBehaviour {
     private float jumpTimer = 0.0f;
     Vector3 gravity;
     [SerializeField]
-    private bool isJumping = false;
+    public bool isGrounded = false;
 
     float storeY;
     private float _slideMultiplier = 1;
 
-    Vector3 moveVelocity;
+    public Vector3 moveVelocity;
 
     private bool onSlope; // is on a slope or not
     private bool slidingSlope; // is on a slope thats too steep
     public float slideSpeed = 0.3f; // slope slide speed
     public float slopeLimit = 50; //Slope limit
-    [SerializeField]
     private float _slopeAngle;
     private Vector3 hitNormal; //orientation of the slope.
 
 
-    CharacterController controller;
+    public CharacterController Controller { get; set; }
 
     [SerializeField]
     private float _gravityScale;
@@ -43,79 +42,38 @@ public class Player : MonoBehaviour {
     void Awake()
     {
         gravity.y = -(2 * jumpHeight) / Mathf.Pow(timeToApex, 2);
+        Controller = GetComponent<CharacterController>();
     }
 
     void Start () {
-        controller = GetComponent<CharacterController>();
+        
 	}
 
- /*   Vector3 Gravity
+    public bool GetGrounded()
     {
-
-        get
+        if (isGrounded)
         {
-
-            Ray ray = new Ray();
-            ray.origin = this.transform.position;
-            ray.direction = Vector3.down;
-
-            //On floor
-            if (Physics.Raycast(ray, this.controller.height * 0.55f))
-            {
-                isJumping = false;
-                //Jump
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    jumpTimer = timeToApex;
-                    isJumping = true;
-                } 
-                
-            }
-
-            //Jump time
-            if (jumpTimer > 0f)
-            {
-                isJumping = true;
-                float height = jumpCurve.Evaluate((timeToApex - jumpTimer) / timeToApex); //Percentage of elapsed jump
-                this.jumpTimer -= Time.deltaTime;
-                var _gravity = Vector3.up * (jumpCurve.Evaluate((timeToApex - jumpTimer) / timeToApex) - height) * jumpHeight;
-                return (_gravity);
-            }
-            else
-            {
-                isJumping = false;
-            }
-            return gravity * Time.deltaTime;
-
+            return true;
         }
-
+        if (Controller.isGrounded || 
+            Controller.collisionFlags == CollisionFlags.CollidedBelow || 
+            Controller.collisionFlags == CollisionFlags.Below)
+            return true;
+        else return false;
     }
-    */
-
-/*	void Update () {
-        storeY = transform.position.y;
-        //Get Player input from Horizontal/Vertical axes
-        Vector3 moveInput = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
-        moveInput = moveInput.normalized * moveSpeed;
-
-        var moveVelocity = (Gravity + moveInput * Time.deltaTime);
-        Debug.Log(moveVelocity.y);
-        controller.Move(moveVelocity);
-    }
-*/
-
+    
     void Update()
     {
         Vector3 input = Vector3.zero;
         input = MoveInput();
         
         if (hitNormal != Vector3.zero) _slopeAngle = Vector3.Angle(Vector3.up, hitNormal);
-        if (!controller.isGrounded) hitNormal = Vector3.zero;
+        if (!Controller.isGrounded) hitNormal = Vector3.zero;
         input = HandleSlopes(input, _slopeAngle); //get movement input from method and send it to handleslopes method. man fuck this code
 
         storeY = transform.position.y;
         moveVelocity += gravity * Time.deltaTime;// * _gravityScale;
-        controller.Move((input + moveVelocity) * Time.deltaTime);
+        Controller.Move((input + moveVelocity) * Time.deltaTime);
     }
 
     public Vector3 MoveInput()
@@ -125,7 +83,7 @@ public class Player : MonoBehaviour {
         moveInput = moveInput.normalized * moveSpeed;
         Vector3 _moveVelocity = new Vector3(0,moveInput.y,0);
 
-        if (controller.isGrounded || controller.collisionFlags == CollisionFlags.CollidedBelow)
+        if (GetGrounded())
             {
             moveVelocity.y = 0;
             if (Input.GetKeyDown(KeyCode.Space))
@@ -146,14 +104,13 @@ public class Player : MonoBehaviour {
 
     private Vector3 HandleSlopes(Vector3 movement, float slopeAngle)
     {
-        onSlope = (Mathf.Abs(slopeAngle)>5f&&controller.isGrounded);
-        Debug.Log(onSlope);
+        onSlope = (Mathf.Abs(slopeAngle)>5f&&Controller.isGrounded);
         slidingSlope = (slopeAngle >= slopeLimit);
 
-        //Character sliding of surfaces
+        //Character on slopes
         if (onSlope) {
             
-            bool ascendSlope, descendSlope;
+            bool ascendSlope, descendSlope; //Determine if character is going up or down a slope.
             if (storeY < transform.position.y)
             {
                 descendSlope = false; ascendSlope = true;
@@ -166,22 +123,25 @@ public class Player : MonoBehaviour {
             {
                 descendSlope = false; ascendSlope = false;
             }
+            //Lose control on slopes too steep
             if (slidingSlope)
             {
                 movement.x += (1f - hitNormal.y) * hitNormal.x * slideSpeed * _slideMultiplier;
                 movement.z += (1f - hitNormal.y) * hitNormal.z * slideSpeed * _slideMultiplier;
-                _slideMultiplier *= 1.1f;
-            }           
+                _slideMultiplier *= 1.05f;
+            }
+            //This works
             if (descendSlope)
             {
                 float descend_moveAmountY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * Vector3.Distance(transform.position, movement);
                 movement = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * new Vector3(movement.x, 0, movement.z);
                 movement.y -= descend_moveAmountY;
             }
+            //this doesn't
             if (ascendSlope)
             {
-                float moveDistance = Mathf.Abs(Vector3.Distance(transform.position, movement));
-                float climb_moveAmountY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+                //float moveDistance = Mathf.Abs(Vector3.Distance(transform.position, movement));
+                //float climb_moveAmountY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
 
                 //movement.y = climb_moveAmountY;
                 //_moveAmount.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance;
@@ -189,6 +149,7 @@ public class Player : MonoBehaviour {
         }
         else
         {
+            //Reset slidespeed multiplier
             _slideMultiplier = 1;
         }
         
@@ -198,4 +159,60 @@ public class Player : MonoBehaviour {
     {
         
     }
+
+    /*   Vector3 Gravity
+   {
+
+       get
+       {
+
+           Ray ray = new Ray();
+           ray.origin = this.transform.position;
+           ray.direction = Vector3.down;
+
+           //On floor
+           if (Physics.Raycast(ray, this.controller.height * 0.55f))
+           {
+               isJumping = false;
+               //Jump
+               if (Input.GetKey(KeyCode.Space))
+               {
+                   jumpTimer = timeToApex;
+                   isJumping = true;
+               } 
+
+           }
+
+           //Jump time
+           if (jumpTimer > 0f)
+           {
+               isJumping = true;
+               float height = jumpCurve.Evaluate((timeToApex - jumpTimer) / timeToApex); //Percentage of elapsed jump
+               this.jumpTimer -= Time.deltaTime;
+               var _gravity = Vector3.up * (jumpCurve.Evaluate((timeToApex - jumpTimer) / timeToApex) - height) * jumpHeight;
+               return (_gravity);
+           }
+           else
+           {
+               isJumping = false;
+           }
+           return gravity * Time.deltaTime;
+
+       }
+
+   }
+   */
+
+    /*	void Update () {
+            storeY = transform.position.y;
+            //Get Player input from Horizontal/Vertical axes
+            Vector3 moveInput = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+            moveInput = moveInput.normalized * moveSpeed;
+
+            var moveVelocity = (Gravity + moveInput * Time.deltaTime);
+            Debug.Log(moveVelocity.y);
+            controller.Move(moveVelocity);
+        }
+    */
+
 }
---
+++ Aquaphobia/Assets/Scripts/RaycastController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class RaycastController : MonoBehaviour {
+
+    [SerializeField][Tooltip("Layers raycasts register hits of")]
+    private LayerMask collisionMask;
+
+    private float skinWidth = .015f;
+    private const float dstBetweenRays = .16f;
+    private int horizontalRayCount = 4;
+    private int verticalRayCount = 4;
+    private int verticalRayCount2 = 4;
+
+    private float horizontalRaySpacing;
+    private float verticalRaySpacing;
+    private float verticalRaySpacing2;
+
+    private Player player;
+    public RaycastOrigins raycastOrigins;
+
+
+    public struct RaycastOrigins
+    {
+        public Vector3 xyz, xyZ;
+        public Vector3 Xyz, XyZ;
+    }
+
+    public void Awake()
+    {
+        if(player==null) player = GetComponent<Player>();
+        
+    }
+
+    void Start () {
+        skinWidth = player.Controller.skinWidth;
+        CalculateRaySpacing();       
+    }
+
+    void Update()
+    {
+        UpdateRaycastOrigins();
+
+        float directionY = Mathf.Sign(player.moveVelocity.y);
+        float rayLength = Mathf.Abs(player.moveVelocity.y) * skinWidth/2 + skinWidth;
+        
+
+        if (Mathf.Abs(player.moveVelocity.y) < 0.5f)
+        {
+            rayLength = 2 * skinWidth;
+        }
+        /* //All four corners
+        Debug.DrawRay(raycastOrigins.xyz, -Vector3.up, Color.red);
+        Debug.DrawRay(raycastOrigins.xyZ, -Vector3.up, Color.blue);
+        Debug.DrawRay(raycastOrigins.Xyz, -Vector3.up, Color.green);
+        Debug.DrawRay(raycastOrigins.XyZ, -Vector3.up, Color.yellow);
+        */
+        for (int k = 0; k < verticalRayCount2; k++)
+        {
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                if (k > 0 && k < verticalRayCount-1 || i > 0 && i < verticalRayCount-1)
+                {
+                    Vector3 rayOrigin = raycastOrigins.xyz + Vector3.forward * k * verticalRaySpacing2;
+                    rayOrigin += Vector3.right * (verticalRaySpacing * i);
+                    RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+                    Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength * 3, Color.yellow);
+                    Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.black);
+                }
+
+            }
+        }
+
+    }
+
+        public void UpdateRaycastOrigins()
+    {
+        Bounds bounds = player.Controller.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.xyz = new Vector3(bounds.min.x, bounds.min.y, bounds.min.z);
+        raycastOrigins.xyZ = new Vector3(bounds.min.x, bounds.min.y, bounds.max.z);
+
+        raycastOrigins.Xyz = new Vector3(bounds.max.x, bounds.min.y, bounds.min.z);
+        raycastOrigins.XyZ = new Vector3(bounds.max.x, bounds.min.y, bounds.max.z);
+
+    }
+
+    public void CalculateRaySpacing()
+    {
+        Bounds bounds = player.Controller.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        float boundsWidth = bounds.size.x;
+        float boundsLength = bounds.size.z;
+        float boundsHeigth = bounds.size.y;
+
+        horizontalRayCount = Mathf.RoundToInt(boundsHeigth / dstBetweenRays);
+        verticalRayCount = Mathf.RoundToInt(boundsWidth / dstBetweenRays);
+        verticalRayCount2 = Mathf.RoundToInt(boundsLength / dstBetweenRays);
+
+        horizontalRaySpacing = boundsHeigth / (horizontalRayCount - 1);
+        verticalRaySpacing = boundsWidth / (verticalRayCount - 1);
+        verticalRaySpacing2 = boundsLength / (verticalRayCount2 - 1);
+    }
+}
