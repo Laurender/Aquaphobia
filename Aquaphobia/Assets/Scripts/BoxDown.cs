81c1baebeadff2606c94cdbd951beb5269f33fd2
c6tonsun <toni.sundell@cs.tamk.fi>
2018-02-20 14:15:47 +0200

    Box down and trouble scene

--- Aquaphobia/Assets/Scripts/BoxDown.cs
+++ Aquaphobia/Assets/Scripts/BoxDown.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class BoxDown : MonoBehaviour {
 
     public Transform end;
+    float slerpTime;
+    float slerpAcceleration;
+    bool slerp = false;
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
+    {
+        if (slerp)
+        {
+            transform.position = Vector3.Slerp(transform.position, end.position, slerpTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, end.rotation, slerpTime);
+        }
+
+        if (slerpTime >= 1 && slerp)
+        {
+            slerp = false;
+            transform.position = end.position;
+            transform.rotation = end.rotation;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            transform.position = end.position;
-            transform.rotation = end.rotation;
+            slerp = true;
+            slerpAcceleration += Time.fixedDeltaTime;
+            slerpTime += slerpAcceleration;
         }
     }
 }
